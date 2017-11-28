d2a5a6ca59a38a4e6d5bddc9ffd4a2710d82e02c
APigin <APigin@APIGIN>
2017-11-28 22:37:41 +0300

    Create new project

---
+++ SeleniumTests/SeleniumTests/Lesson1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumTests
+{
+    [TestClass]
+    public class Lesson1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

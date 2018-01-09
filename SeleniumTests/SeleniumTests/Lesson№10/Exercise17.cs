225396a74db6a96c303f04404f06a70434f0bf21
APigin <APigin@APIGIN>
2018-01-09 15:16:26 +0300

    17,18

---
+++ SeleniumTests/SeleniumTests/Lesson№10/Exercise17.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests.Lesson10
+{
+    [TestClass]
+    public class Exercise17
+    {
+        public void EnterAdminPanel()
+        {
+            var driver = Driver_Chrome.GetInstance_Chrome;
+
+            try
+            {
+                new WebDriverWait(driver, TimeSpan.FromSeconds(15)).Until(ExpectedConditions.ElementExists((By.Name("login_form"))));
+
+                IWebElement Login = driver.FindElement(By.Name("username"));
+                IWebElement Password = driver.FindElement(By.Name("password"));
+                IWebElement LoginButton = driver.FindElement(By.Name("login"));
+
+                Login.SendKeys("admin");
+                Password.SendKeys("admin");
+                LoginButton.Click();
+                new WebDriverWait(driver, TimeSpan.FromSeconds(15)).Until(ExpectedConditions.ElementExists((By.Id("sidebar"))));
+
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message + "\n" + e.InnerException.Message);
+            }
+        }
+
+        [TestMethod]
+        public void CheckCatalog()
+        {
+            //******************************
+            /*
+            чтобы проверить можно на время теста переименовать папку
+            C:\xampp\htdocs\litecart\ext\trumbowyg  -> C:\xampp\htdocs\litecart\ext\trumbowyg2
+            тогда будут валиться сообщения при входе на страницу продукта
+            */
+            //******************************
+            var driver = Driver_Chrome.GetInstance_Chrome;
+            driver.Navigate().GoToUrl("http://localhost:8080/litecart/admin");
+            if (driver.Url.Contains("login"))
+            {
+                EnterAdminPanel();
+            }
+            try
+            {
+                var logmessages = new Dictionary<string, IReadOnlyCollection<LogEntry>>();
+                driver.Navigate().GoToUrl("http://localhost:8080/litecart/admin/?app=catalog&doc=catalog&category_id=1");
+                var listOfProducts = driver.FindElements(
+                    By.XPath(".//table[@class='dataTable']//td/img/../a[contains(@href,'product_id')]")).Select(x => x.Text).ToList();
+                foreach (var product in listOfProducts)
+                {
+                    driver.Navigate().GoToUrl("http://localhost:8080/litecart/admin/?app=catalog&doc=catalog&category_id=1");
+                    driver.FindElement(By.XPath(".//table[@class='dataTable']//td/img/../a[contains(text(),'" + product + "')]")).Click();
+                    var logs = driver.Manage().Logs.GetLog("browser");
+                    if (logs.Count > 0)
+                    {
+                        logmessages.Add(product, logs);
+                    }
+                }
+                if (logmessages.Count > 0)
+                {
+                    foreach (var log in logmessages)
+                    {
+                        Console.WriteLine(log.Key);
+                        foreach (var logMessage in log.Value)
+                        {
+                            Console.WriteLine(logMessage.Level + "\t" + logMessage.Message);
+                        }
+                    }
+                    Assert.Fail("There are some messages in logs!");
+                }
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+        [TestCleanup]
+        public void Clean()
+        {
+            Driver_Chrome.GetInstance_Chrome.Quit();
+        }
+    }
+}
---
+++ SeleniumTests/SeleniumTests/Lesson№10/Exercise18.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Fiddler;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using Proxy = OpenQA.Selenium.Proxy;
+
+
+namespace SeleniumTests.Lesson_10
+{
+    [TestClass]
+    public class Exercise18
+    {
+        [TestMethod]
+        public void test()
+        {
+            var seleniumProxy = new Proxy { HttpProxy = "localhost:8877", SslProxy = "localhost:8877" };
+            var option = new FirefoxOptions {Proxy = seleniumProxy};
+            var slenium = new FirefoxDriver(option);
+
+            const FiddlerCoreStartupFlags fiddlerStartUpFlags = FiddlerCoreStartupFlags.DecryptSSL & FiddlerCoreStartupFlags.AllowRemoteClients & FiddlerCoreStartupFlags.CaptureFTP & FiddlerCoreStartupFlags.ChainToUpstreamGateway & FiddlerCoreStartupFlags.MonitorAllConnections & FiddlerCoreStartupFlags.CaptureLocalhostTraffic;
+            FiddlerApplication.Prefs.SetStringPref("fiddler.config.path.makecert", @"d:\..\Makecert.exe");//To define the MakeCert.exe path manually.
+            FiddlerApplication.Prefs.SetBoolPref("fiddler.network.streaming.abortifclientaborts", true);//Abort session when client abort
+            FiddlerApplication.Startup(8877, fiddlerStartUpFlags);
+            FiddlerApplication.AfterSessionComplete += delegate (Session targetSession)
+            {
+                Console.WriteLine("{0}\t{1}",targetSession.responseCode,targetSession.fullUrl);
+            };
+            slenium.Navigate().GoToUrl("http://google.com");
+            FiddlerApplication.oProxy.PurgeServerPipePool();//Run this between tests to make sure the new test will start "clean"
+            slenium.Quit();
+        }
+    }
+}
